3e3c0a893f17c233248e9248e5a474becd59f69b
Dr-hydra <116339679+Dr-hydra@users.noreply.github.com>
2026-09-11 11:21:23 +0800

    custom-model: wire in-place generation recommit probe

--- native/modules/custom_model/module.cpp
+++ native/modules/custom_model/module.cpp
@@ -8,4 +8,5 @@
 #include "module_poc2_part_03.inc"
 } // closes the Shutdown() body continued from module_poc2_part_03.inc
 #include "module_generation_diag.inc"
+#include "module_generation_recommit_diag.inc"
 #include "module_poc2_part_04.inc"
