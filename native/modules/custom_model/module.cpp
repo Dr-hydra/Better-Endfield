035a752f8310d0a77499cb9f03a6d5c587c01ed8
Dr-hydra <116339679+Dr-hydra@users.noreply.github.com>
2026-09-11 12:46:15 +0800

    custom-model: replace A/B diagnostics with F12 state dump

--- native/modules/custom_model/module.cpp
+++ native/modules/custom_model/module.cpp
@@ -7,8 +7,5 @@
 #include "module_poc2_part_02.inc"
 #include "module_poc2_part_03.inc"
 } // closes the Shutdown() body continued from module_poc2_part_03.inc
-#include "module_generation_diag.inc"
-#include "module_generation_recommit_diag.inc"
-#include "module_twin_mesh_diag.inc"
-#include "module_pair_mode_diag.inc"
+#include "module_f12_state_dump.inc"
 #include "module_poc2_part_04.inc"
